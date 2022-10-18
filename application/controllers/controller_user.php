7f55540a9c5acfeadc7a06ddaaa6bc2e4c9e0e16
Egor Shamov <eshamov030316@gmail.com>
2022-10-19 06:21:57 +0700

    Начал работать с архитектурой. Пишу код по гайду MVC

---
+++ application/controllers/controller_user.php
@@ -0,0 +1,14 @@
+<?php
+
+class Controller_User extends Controller {
+    function __construct()
+    {
+        $this->model = new Model_User();
+        $this->view = new View();
+    }
+    function action_index()
+    {
+        $data = $this->model->get_data();
+        $this->view->generate('view_user.php', 'view_template.php', $data);
+    }
+}
